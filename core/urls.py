383dc591886b16c0bb0d5f9d2bb8d553bf7a522e
meraj <merajpiri83@gmail.com>
2024-09-08 20:49:56 +0330

    initial config

---
+++ core/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.views.static import serve
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema = get_schema_view(
+    openapi.Info(
+        title="kimiya saneat",
+        description="backend of kimiyasaneat built with django and django rest frame work",
+        default_version="v1"
+    ),
+    public=True
+)
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+
+    path('',schema.with_ui('swagger',cache_timeout=0),name="swagger"),
+
+    re_path('^media/(?P<path>.*)$',serve,{'document_root' : settings.MEDIA_ROOT}),
+]
